6835cae2ad6ebf87f34e5ce6b3b0fa2732ca3200
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-22 18:26:56 +0300

    Added localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Counter } from './Counter';
+import { Counter } from './Display';
 import { Buttons } from './Buttons';
 
 function App() {
   let [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    let valueAsString = localStorage.getItem('countValue');
+    if (valueAsString) {
+      let newValue = JSON.parse(valueAsString);
+      setCount(newValue);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('countValue', JSON.stringify(count));
+  }, [count]);
 
   const incValue = () => {
     let countPlus = count + 1;
     if (countPlus >= 5) {
       countPlus = 5;
     }
-    setCount(countPlus);
+    let newIncValue = setCount(countPlus);
   };
 
   const resetValue = () => {
